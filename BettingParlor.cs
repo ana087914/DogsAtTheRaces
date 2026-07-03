fdf88311c485d4225b641dd4ffa900eceef2bbbb
simplyana08 <ana.t08@yahoo.com>
2026-07-03 13:23:46 +0200

    Initialized Joe

--- BettingParlor.cs
+++ BettingParlor.cs
@@ -9,6 +9,12 @@ Guy[] guys = new Guy[3];
     public BettingParlor()
     {
         InitializeComponent();
+        guys[0] = new Guy();
+        guys[0].Name = "Joe";
+        guys[0].Cash = 50;
+        guys[0].MyRadioButton = rb_Guy1;
+        guys[0].MyLabel = lb_guy1BetLabel;
+        guys[0].ClearBet();
     }
 
 
