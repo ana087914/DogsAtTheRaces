8fa2c9cb4d3bc9dff3dc30e5dd8012bf14e9ee4e
simplyana08 <ana.t08@yahoo.com>
2026-07-03 10:04:29 +0200

    Added Guy fields

--- Guy.cs
+++ Guy.cs
@@ -1,6 +1,14 @@
-﻿namespace DogsAtTheRaces
+﻿using System.Windows.Forms;
+
+namespace DogsAtTheRaces
 {
     public class Guy
     {
+        public string Name;
+        public Bet MyBet;
+        public int Cash;
+
+        public RadioButton MyRadioButton;
+        public Label MyLabel;
     }
 }
