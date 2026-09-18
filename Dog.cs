819225554a641b885d26b56a7eec16685f64f403
simplyana08 <ana.t08@yahoo.com>
2026-09-18 14:30:32 +0200

    Changed Dog fields to properties

--- Dog.cs
+++ Dog.cs
@@ -10,16 +10,44 @@ namespace DogsAtTheRaces
         public PictureBox MyPictureBox;
         public int Location;
         public Random Randomizer;
+        public int StartingPosition
+        {
+            get{ return startingPosition;}
+            set { startingPosition=value;}
+        }
 
+        public int RacetrackLength
+        {
+            get {return racetrackLength;}
+            set {racetrackLength = value;}
+        }
+
+        public PictureBox MyPictureBox
+        {
+            get {return myPictureBox;}
+            set {myPictureBox=value;}
+        }
+
+        public int Location
+        {
+            get {return location;}
+            set {location=value;}
+        }
+
+        public Random Randomizer
+        {
+            get {return randomizer;}
+            set {randomizer=value;}
+        }
         public void TakeStartingPosition()
         {
-            Location = 0;
-            MyPictureBox.Left = StartingPosition;
+            Location= 0;
+            MyPictureBox.Left= StartingPosition;
         }
         public bool Run()
         {
-            Location = Location + Randomizer.Next(1, 5);
-            MyPictureBox.Left = StartingPosition + Location;
+            Location= Location+Randomizer.Next(1, 5);
+            MyPictureBox.Left= StartingPosition +Location;
 
             if (Location >= RacetrackLength)
             {
